1209533ec7fc3b5b310469ec369d13eb9e085f2f
Rishi <rishikm215@gmail.com>
2024-10-08 20:24:25 +0530

    RESUME

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,63 +1,7 @@
-
-function faultycalc(a, b) {
-    const ans = "y"
-    while (ans =="y") {
-        var r = Math.random()
-        if (r < 0.1) {
-            console.log("1.Addition\n" +
-                "2.Subtraction\n" +
-                "3.Multiplication\n" +
-                "4.Division")
-            var a = prompt("Enter Your Choice (Numeric Value) : ")
-            if (a == 1) {
-                console.log(a - b)
-            }
-
-            else if (a == 2) {
-                console.log(a / b)
-            }
-
-            else if (a == 3) {
-                console.log(a + b)
-            }
-
-            else if (a == 4) {
-                console.log(a ** b)
-            }
-
-            else {
-                console.log("Oops! Wrong input!")
-            }
-        }
-        else {
-            console.log("1.Addition\n" +
-                "2.Subtraction\n" +
-                "3.Multiplication\n" +
-                "4.Division")
-            var a = prompt("Enter Your Choice (Numeric Value) : ")
-            if (a == 1) {
-                console.log(a + b)
-            }
-
-            else if (a == 2) {
-                console.log(a - b)
-            }
-
-            else if (a == 3) {
-                console.log(a * b)
-            }
-
-            else if (a == 4) {
-                console.log(a / b)
-            }
-
-            else {
-                console.log("Oops! Wrong input!")
-            }
-        }
-
-    }
-    ans = prompt("want to execute more ? [y/n]")
+let n = prompt("Enter the Number : ")
+let prod = 1
+while (n!=0){
+    prod*=n
+    n-=1
 }
-
-faultycalc(9,10)
+console.log("Factorial : "+prod)
