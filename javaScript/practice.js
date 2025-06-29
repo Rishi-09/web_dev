463ecb0a248a9f1d9eacfb96ba111411a77a2875
Rishi <rishikm215@gmail.com>
2025-06-29 17:29:37 +0530

    auto random colours

--- javaScript/practice.js
+++ javaScript/practice.js
@@ -1,6 +1,8 @@
 const boxes = document.querySelectorAll('.box');
 
 boxes.forEach((box) => {    
-    box.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-    box.style.color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    setInterval(() => {
+        box.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+        box.style.color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    },500);
 } );
