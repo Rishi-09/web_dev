5d55e15c52ced1bd9b482cd505dd6aab79d18141
Rishi <rishikm215@gmail.com>
2025-07-12 19:16:34 +0530

    re-structured minor projects

--- javaScript/anotherMiniProject.js
+++
@@ -1,6 +0,0 @@
-const rect = document.querySelector(".rect");
-rect.addEventListener("mouseover",function(details){
-    this.style.background-image = );
-});
-
-
--- javaScript/practice.js
+++
@@ -1,8 +0,0 @@
-const boxes = document.querySelectorAll('.box');
-
-boxes.forEach((box) => {    
-    setInterval(() => {
-        box.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-        box.style.color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-    },500);
-} );
