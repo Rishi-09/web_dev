bbe18fa582ba09cf8449f00a876cf7ab312e4167
Rishi <rishikm215@gmail.com>
2025-08-01 20:09:01 +0530

    added a raw to do app

---
+++ javaScript/script.js
@@ -0,0 +1,22 @@
+const btn = document.body.querySelector(".btn");
+const input = document.body.querySelector("input");
+const list = document.body.querySelector(".list");
+
+btn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    let li = document.createElement("li");
+    let delBtn = document.createElement("button");
+    delBtn.innerText="delete";
+    delBtn.classList.add("delete")
+    li.style.margin="10px"; 
+    li.innerText=`${input.value}`;
+    list.append(li);
+    li.append(delBtn);
+    input.value="";
+});
+
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+ }
+});
