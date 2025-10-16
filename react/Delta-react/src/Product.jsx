0cfb4e16909186f5399415a5a2f681f06098fefc
Rishi <rishikm215@gmail.com>
2025-10-16 18:14:29 +0530

    rendering array with html elements

--- react/Delta-react/src/Product.jsx
+++ react/Delta-react/src/Product.jsx
@@ -4,9 +4,10 @@ function Product({title,price,features}){
     return (
         <div className='Product' >
             <h3>{title}</h3>
-            <p>{title} Description</p>
+            <p>{title}: Description</p>
             <p>Price:{price}</p>
-            <p>{features}</p>
+            {price>20000 && <p>Discount:5%</p>}
+            <p>{features.map((feature)=><li>{feature}</li>)}</p>
         </div>
     )
 }
--- react/Delta-react/src/ProductTab.jsx
+++ react/Delta-react/src/ProductTab.jsx
@@ -1,11 +1,12 @@
 import Product from "./Product.jsx";
 
 function ProductTab(){
+    let options=["hi-tech","durable"];
     return (
         <>
-            <Product title="phone"     price={20000}   features={["hi-tech","durable"]} />
-            <Product title="tablet"    price={25000}   features={["hi-tech","durable"]} />
-            <Product title="Computer"  price={100000}  features={["hi-tech","durable"]}  />
+            <Product title="phone"     price={20000}   features={options} />
+            <Product title="tablet"    price={25000}   features={options} />
+            <Product title="Computer"  price={100000}  features={options}  />
         </>
     )
 }
