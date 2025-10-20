b435bf16967488173e799379fad6968395cd632d
Rishi <rishikm215@gmail.com>
2025-10-20 16:34:05 +0530

    added mark as done/Not-done

--- react/to-do-App/src/ToDoApp.jsx
+++ react/to-do-App/src/ToDoApp.jsx
@@ -1,16 +1,76 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
 export default function ToDoApp() {
-  let [tasks, settasks] = useState(["task1"]);
+  let [tasks, settasks] = useState([
+    { task: "Your Tasks appear Here", id: uuidv4(),isDone:false },
+  ]);
+
   let [newTask, setnewTask] = useState("");
   let addTask = () => {
-    settasks([...tasks,newTask]);
+    settasks((prevTasks) => {
+      return [...prevTasks, { task: newTask, id: uuidv4(),isDone:false }];
+    });
     setnewTask("");
   };
 
   let updateTask = (event) => {
     setnewTask(event.target.value);
-};
+  };
 
+  let delTask = (id) => {
+    let copy = tasks.filter((task)=>task.id!=id);
+    settasks((tasks)=>{
+       return [...copy];
+    });
+  };
+
+  let markAllDone=()=>{
+    settasks((prevTasks)=>prevTasks.map((task)=>{
+        return {
+            ...task,
+            isDone:true,
+        };
+    }))
+  }
+
+  let markDone=(id)=>{
+    settasks((prevTasks)=>prevTasks.map((task)=>{
+        if(task.id===id){
+            return {
+            ...task,
+            isDone:true,
+        }
+        }else{
+            return {
+                ...task
+            }
+        }
+    }))
+  }
+  let markAllNotDone=()=>{
+    settasks((prevTasks)=>prevTasks.map((task)=>{
+        return {
+            ...task,
+            isDone:false,
+        };
+    }))
+  }
+
+  let markNotDone=(id)=>{
+    settasks((prevTasks)=>prevTasks.map((task)=>{
+        if(task.id===id){
+            return {
+            ...task,
+            isDone:false,
+        }
+        }else{
+            return {
+                ...task
+            }
+        }
+    }))
+  }
   return (
     <>
       <input
@@ -25,11 +85,25 @@ export default function ToDoApp() {
       <br /> <br />
       <hr />
       <h1>Tasks To Do</h1>
-      <ul>
+      <ul className="list"  >
         {tasks.map((task) => {
-          return <li>{task}</li>;
+          return (
+            <li key={task.id}>
+              <span style={task.isDone?{textDecorationLine:"line-through"}:{}} >
+                {task.task} &nbsp;&nbsp;
+              </span>
+              <span>
+                <button onClick={() => delTask(task.id)}> Delete </button> &nbsp;&nbsp;&nbsp;
+                <button onClick={() => markDone(task.id)}> Mark as Done </button> &nbsp;&nbsp;&nbsp;
+                <button onClick={() => markNotDone(task.id)}> Mark as Not Done </button> &nbsp;&nbsp;&nbsp;
+              </span>
+            </li> 
+          );
         })}
       </ul>
+      <button onClick={markAllDone} >Mark All As Done</button>&nbsp;&nbsp;&nbsp;
+      <button onClick={markAllNotDone} >Mark All As Not Done</button>&nbsp;&nbsp;&nbsp;
+      
     </>
   );
 }
